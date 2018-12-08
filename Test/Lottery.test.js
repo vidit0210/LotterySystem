8634e957478288b3118cf1cb8e4cb9245235bea9
Vidit Shah <vidit0210@gmail.com>
2018-12-09 05:19:46 +0530

    testv2

--- Test/Lottery.test.js
+++ Test/Lottery.test.js
@@ -9,8 +9,44 @@ let account;
 
 beforeEach(async()=>{
     account = await web3.eth.getAccounts();
-    lottery = await web3.eth.Contract(JSON.parse(interface))
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
                 .deploy({data:bytecode})
                 .send({from:account[0],gas:'1000000'});
    
+});
+
+describe("Lottery Contract",()=>{
+    it('Contract Deployed?',()=>
+    {
+        assert.ok(lottery.options.address);
+    })
+
+    it('Can Enter?', async() => {
+        await lottery.methods.enter().send(
+            {
+                from:account[0],
+                value:web3.utils.toWei('0.02','ether')
+            }
+        )
+        await lottery.methods.enter().send(
+            {
+                from:account[1],
+                value:web3.utils.toWei('0.02','ether')
+            }
+        )
+        await lottery.methods.enter().send(
+            {
+                from:account[2],
+                value:web3.utils.toWei('0.02','ether')
+            }
+        )
+        const players = await lottery.methods.getAllplayers().call({
+            from:account[0]
+        }
+        );
+        assert.equal(account[0],players[0]);
+        assert.equal(3,players.length);
+    });
+
+    
 });
